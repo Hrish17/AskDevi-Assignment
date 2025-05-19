7f13e70af8369a9396ce3e6b345034b7acd5ac19
HSangwaiya <ashsangwaiya@gmail.com>
2025-05-19 14:58:27 +0530

    updates

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ["DJANGO_SECRET_KEY"]
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["askdevi-assignment.onrender.com/",]
+ALLOWED_HOSTS = ["askdevi-assignment.onrender.com",]
 
 # Application definition
 
